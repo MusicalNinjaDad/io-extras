fad2658d0a5365f4e406746fc4984cdeca06caeb
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-03 18:42:41 +0000

    directly print io_lifetimes_use_std

--- build.rs
+++ build.rs
@@ -8,15 +8,12 @@ fn main() -> Result<()> {
     ac.emit_unstable_feature(can_vector, &allowed_features); // https://github.com/rust-lang/rust/issues/69941
     ac.emit_unstable_feature(write_all_vectored, &allowed_features); // https://github.com/rust-lang/rust/issues/70436
 
-    use_feature("io_lifetimes_use_std");
+    println!("cargo:rustc-check-cfg=cfg(io_lifetimes_use_std)");
+    println!("cargo:rustc-cfg=io_lifetimes_use_std");
 
     // Don't rerun this on changes other than build.rs, as we only depend on
     // the rustc version.
     println!("cargo:rerun-if-changed=build.rs");
 
     Ok(())
-}
-
-fn use_feature(feature: &str) {
-    println!("cargo:rustc-cfg={}", feature);
 }
