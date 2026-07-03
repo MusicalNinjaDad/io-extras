6a52131cfa7b96b0807edd34c5e9afbf325b6776
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-03 18:33:53 +0000

    use unstable_... cfgs

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,8 @@
 //!   types with one or two I/O resources, for reading and for writing.
 
 #![deny(missing_docs)]
-#![cfg_attr(can_vector, feature(can_vector))]
-#![cfg_attr(write_all_vectored, feature(write_all_vectored))]
+#![cfg_attr(unstable_can_vector, feature(can_vector))]
+#![cfg_attr(unstable_write_all_vectored, feature(write_all_vectored))]
 #![cfg_attr(target_os = "wasi", feature(wasi_ext))]
 
 pub mod borrowed;
